f0ce1c0ad07a323ce4127f88731f9f46e595c558
Enoh <eno.col@gmail.com>
2023-07-25 16:50:21 +0100

    Edit index.js for dynamic html input

--- index.js
+++ index.js
@@ -17,3 +17,55 @@ for (let menu of menuitems) {
     menulist.classList.remove("menu-visible");
   });
 }
+
+const popupwindow = document.querySelector(".popupwindow");
+
+const div = document.createElement("div");
+div.innerHTML = `<div class="window">
+                 <div><i class="fa-regular fa-x icon-close"></i></div>
+        
+          <h1 class="">Tonic</h1>
+          <div class="">
+            <div class="">
+              <span>Canopy</span>
+              <ul class="">
+                <li class="">Back End Dev</li>
+                <li class="">2015</li>
+              </ul>
+            </div>
+
+            <div class="">
+          <img src="./Images/Snapshoot-Portfolio (2).png" alt="" />
+              </div>
+
+            <p class="">
+              A daily selection of privately personalized reads; no accounts or
+              sign-ups required.
+            </p>
+            <ul class="">
+              <li class="">HTML</li>
+              <li class="">Css</li>
+              <li class="">JavaScript</li>
+            </ul>
+            <button>See Project</button>
+          </div>
+        </div>
+      </div>`;
+
+div.className = "popupcontainer";
+
+popupwindow.append(div);
+
+const popclose = document.querySelector(".icon-close");
+popclose.addEventListener("click", function () {
+  popupwindow.classList.remove("visible");
+  console.log("closed");
+});
+
+const projectbtn = document.querySelectorAll(".btn");
+for (let btn of projectbtn) {
+  btn.addEventListener("click", function (e) {
+    e.stopPropagation();
+    popupwindow.classList.add("visible");
+  });
+}
