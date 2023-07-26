3a5bbd40a219a48ec47913da5c1fadff9e887776
Enoh <eno.col@gmail.com>
2023-07-26 03:46:48 +0100

    Add JavaScript for detsils pop up window

--- index.js
+++ index.js
@@ -24,30 +24,32 @@ const div = document.createElement("div");
 div.innerHTML = `<div class="window">
                  <div><i class="fa-regular fa-x icon-close"></i></div>
         
-          <h1 class="">Tonic</h1>
+          <h1 class="heading m-b"></h1>
           <div class="">
-            <div class="">
-              <span>Canopy</span>
-              <ul class="">
-                <li class="">Back End Dev</li>
-                <li class="">2015</li>
+            <div class="canopy">
+              <span></span>
+              <ul class="canopy-list">
+                <li class="backend"></li>
+                <li class="year"></li>
               </ul>
             </div>
 
-            <div class="">
-          <img src="./Images/Snapshoot-Portfolio (2).png" alt="" />
-              </div>
+           <div class="img">
+              <img src= alt="" />
+            </div>
 
             <p class="">
               A daily selection of privately personalized reads; no accounts or
               sign-ups required.
             </p>
-            <ul class="">
+            <ul class="list-B">
               <li class="">HTML</li>
               <li class="">Css</li>
               <li class="">JavaScript</li>
             </ul>
-            <button>See Project</button>
+            <div>
+             <button>See Live</button>
+             <button>See source</button></div>
           </div>
         </div>
       </div>`;
@@ -59,7 +61,6 @@ popupwindow.append(div);
 const popclose = document.querySelector(".icon-close");
 popclose.addEventListener("click", function () {
   popupwindow.classList.remove("visible");
-  console.log("closed");
 });
 
 const projectbtn = document.querySelectorAll(".btn");
@@ -67,5 +68,43 @@ for (let btn of projectbtn) {
   btn.addEventListener("click", function (e) {
     e.stopPropagation();
     popupwindow.classList.add("visible");
+
+    const array = [];
+
+    const card = e.target.parentNode.parentNode.parentNode;
+    const card4 = document.querySelector(".card");
+    console.log("car4  =", card4);
+
+    const h1 = card.querySelector(".h-1").innerHTML;
+    const span = card.querySelector(".project span").innerHTML;
+    const backend = card.querySelectorAll(".listA-items");
+    const img = card.querySelector(".cards img").src;
+    const paragraph = card4.querySelector(".p-1");
+
+    const portfolio = card.querySelectorAll(".listB-items");
+
+    array[0] = h1;
+    array[1] = span;
+    array[2] = { list1: backend[0].innerHTML, list2: backend[1].innerHTML };
+    array[3] = img;
+    array[4] = paragraph;
+    array[5] = {
+      portfolio1: portfolio[0].innerHTML,
+      portfolio2: portfolio[1].innerHTML,
+      portfolio3: portfolio[2].innerHTML,
+    };
+
+    const heading = document.querySelector(".heading");
+    const canopy = document.querySelector(".canopy span");
+    const backn = document.querySelector(".backend");
+    const year = document.querySelector(".year");
+    let image = document.querySelector(".img img");
+    image.src = array[3];
+    year.innerHTML = array[2].list2;
+    backn.innerHTML = array[2].list1;
+    canopy.innerHTML = array[1];
+    heading.innerHTML = array[0];
+
+    console.log(array);
   });
 }
