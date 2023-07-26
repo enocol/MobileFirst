f90e0fe680745b2e68a1bcd4dcda6270988934a0
Enoh <eno.col@gmail.com>
2023-07-26 14:09:31 +0100

    Edit index.js

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-const hamburger = document.querySelector(".fa");
-const menulist = document.querySelector(".menu-list");
-const menuitems = document.querySelectorAll(".menu-item");
-const closebutton = document.querySelector(".fa-x");
+/* eslint-disable prefer-destructuring */
+/* eslint-disable no-restricted-syntax */
+const hamburger = document.querySelector('.fa');
+const menulist = document.querySelector('.menu-list');
+const menuitems = document.querySelectorAll('.menu-item');
+const closebutton = document.querySelector('.fa-x');
 
-hamburger.addEventListener("click", function () {
-  menulist.classList.add("menu-visible");
-  for (let element of menuitems) element.classList.add("menucolor");
+hamburger.addEventListener('click', () => {
+  menulist.classList.add('menu-visible');
+  for (const element of menuitems) element.classList.add('menucolor');
 });
 
-closebutton.addEventListener("click", function () {
-  menulist.classList.remove("menu-visible");
+closebutton.addEventListener('click', () => {
+  menulist.classList.remove('menu-visible');
 });
 
-for (let menu of menuitems) {
-  menu.addEventListener("click", function () {
-    menulist.classList.remove("menu-visible");
+for (const menu of menuitems) {
+  menu.addEventListener('click', () => {
+    menulist.classList.remove('menu-visible');
   });
 }
 
-const popupwindow = document.querySelector(".popupwindow");
+const popupwindow = document.querySelector('.popupwindow');
 
-const div = document.createElement("div");
+const div = document.createElement('div');
 div.innerHTML = `<div class="window">
                  <div><i class="fa-regular fa-x icon-close"></i></div>
         
@@ -60,33 +62,31 @@ div.innerHTML = `<div class="window">
         </div>
       </div>`;
 
-div.className = "popupcontainer";
+div.className = 'popupcontainer';
 
 popupwindow.append(div);
 
-const popclose = document.querySelector(".icon-close");
-popclose.addEventListener("click", function () {
-  popupwindow.classList.remove("visible");
+const popclose = document.querySelector('.icon-close');
+popclose.addEventListener('click', () => {
+  popupwindow.classList.remove('visible');
 });
 
-const projectbtn = document.querySelectorAll(".btn");
-for (let btn of projectbtn) {
-  btn.addEventListener("click", function (e) {
+const projectbtn = document.querySelectorAll('.btn');
+for (const btn of projectbtn) {
+  btn.addEventListener('click', (e) => {
     e.stopPropagation();
-    popupwindow.classList.add("visible");
+    popupwindow.classList.add('visible');
 
     const array = [];
 
     const card = e.target.parentNode.parentNode.parentNode;
 
-    const name = card.querySelector(".h-1").innerHTML;
-    const span = card.querySelector(".project span").innerHTML;
-    const technologies = card.querySelectorAll(".listA-items");
-    const featureimage = card.querySelector(".cards img").src;
-    const decription = card.querySelector(".p-1");
-    console.log(decription);
+    const name = card.querySelector('.h-1').innerHTML;
+    const span = card.querySelector('.project span').innerHTML;
+    const technologies = card.querySelectorAll('.listA-items');
+    const featureimage = card.querySelector('.cards img').src;
 
-    const portfolio = card.querySelectorAll(".listB-items");
+    const portfolio = card.querySelectorAll('.listB-items');
 
     array[0] = name;
     array[1] = span;
@@ -110,19 +110,17 @@ Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the re
       portfolio3: portfolio[2].innerHTML,
     };
 
-    const heading = document.querySelector(".heading");
-    const canopy = document.querySelector(".canopy span");
-    const backn = document.querySelector(".backend");
-    const year = document.querySelector(".year");
-    let image = document.querySelector(".img img");
-    const p1 = document.querySelector(".p1");
+    const heading = document.querySelector('.heading');
+    const canopy = document.querySelector('.canopy span');
+    const backn = document.querySelector('.backend');
+    const year = document.querySelector('.year');
+    const image = document.querySelector('.img img');
+    const p1 = document.querySelector('.p1');
     image.src = array[3];
     year.innerHTML = array[2].list2;
     backn.innerHTML = array[2].list1;
     canopy.innerHTML = array[1];
     heading.innerHTML = array[0];
     p1.innerHTML = array[4];
-
-    console.log(array);
   });
 }
